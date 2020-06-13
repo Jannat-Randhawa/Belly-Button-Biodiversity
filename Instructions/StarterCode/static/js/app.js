11322ff7a6e4d1046872c6857e5166b8d41ab525
Jannat-Randhawa <58931852+Jannat-Randhawa@users.noreply.github.com>
2020-06-13 13:11:07 -0400

    bar graph not working

--- Instructions/StarterCode/static/js/app.js
+++ Instructions/StarterCode/static/js/app.js
@@ -1,18 +1,42 @@
-var dropDown = d3.select("#selDataset"); 
+var dropDown = d3.select("#selDataset").on("change", optionChanged); 
 
 
 function top10bar (ID){
     d3.json("samples.json").then((data) => {
         var samples = data.samples
-        console.log(samples);
-        ID =  parseInt(ID)
+        // console.log(samples);
         var idData = samples.filter(sample => parseInt(sample.id)=== ID); 
-        console.log(idData)
-        var idOtuData = idData[0].otu_ids.slice(0,10); 
-        console.log(idOtuData)
+        // console.log(idData)
+        var idOtu = idData[0].otu_ids.slice(0,10); 
+        // console.log(idOtu)
+        var idOturev = idOtu.reverse();
+        console.log(idOturev);
+        var labels = idOtu.map(i => "OTU_" + i);
+        console.log(labels);
+        var otuLabels = idData[0].otu_labels.slice(0,10);
+        console.log(otuLabels)
         var topSamples = idData[0].sample_values.slice(0,10);
+        console.log(topSamples);
+        var topSamplesrev = topSamples.reverse();
+        console.log(topSamplesrev);
 
+        var trace = {
+            x: topSamplesrev ,
+            y: labels,
+            text: otuLabels,
+            type: "bar",
+            orientation: "h"
+        };
+        
+        var data = [trace];
+        
+        var layout = {
+            title: "Top Ten OTU ID"
+        };
+        
+        Plotly.newPlot("bar", data, layout);
     });
+
 };
 
 function init() {
@@ -27,12 +51,17 @@ function init() {
           });
     });
     top10bar(940)
-}
+
+};
 
 function optionChanged(change){
     top10bar(change)
-}
+    
+}; 
+
 init()
+
+
 
 
 
