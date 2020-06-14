74e9898ddafcf8faa9df04c8a74fc2f7ba65f467
Jannat-Randhawa <58931852+Jannat-Randhawa@users.noreply.github.com>
2020-06-14 17:31:36 -0400

    testing code

--- Instructions/StarterCode/static/js/app.js
+++ Instructions/StarterCode/static/js/app.js
@@ -1,109 +1,12 @@
-var dropDown = d3.select("#selDataset")
+var globalJson = null;
+var dropDown = d3.select("#selDataset");
 
-function top10bar (ID){
+
+function init () {
     d3.json("samples.json").then((data) => {
-        var samples = data.samples
-        // console.log(samples);
-        var idData = samples.filter(sample => parseInt(sample.id)=== parseInt(ID)); 
-        // console.log(idData)
-        var otuIDs = idData[0].otu_ids;
-        console.log(otuIDs);
-        var idOtu = idData[0].otu_ids.slice(0,10); 
-        // console.log(idOtu)
-        var IDotu = idOtu.map(i => "OTU_" + i);
-        console.log(IDotu);
-        var otuLabels = idData[0].otu_labels.slice(0,10);
-        console.log(otuLabels);
-        var sampleValues = idData[0].sample_values;
-        console.log(sampleValues);
-        var topSamples = idData[0].sample_values.slice(0,10);
-        console.log(topSamples);
-        var topSamplesrev = topSamples.reverse();
-        console.log(topSamplesrev);
-        // var wfreq = data.metadata.map(wash => (wash.wfreq));
-        // console.log(wfreq);
 
-        var trace = {
-            x: topSamplesrev ,
-            y: IDotu,
-            text: otuLabels,
-            type: "bar",
-            orientation: "h"
-        };
-        
-        var data = [trace];
-        
-        var layout = {
-            title: "Top Ten OTU ID"
-        };
-        
-        Plotly.newPlot("bar", data, layout);
-
-        var trace2 = {
-            x: otuIDs,
-            y: sampleValues,
-            text: otuLabels,
-            mode: 'markers',
-            marker: {
-                size: sampleValues,
-                color: otuIDs
-            }
-        };
-
-        var data2 = [trace2]; 
-
-        var layout2 = {
-            title: 'Bubble Chart',
-            height: 600,
-            width: 1000
-        };
-        
-        Plotly.newPlot('bubble', data2, layout2);
-    });
-    
-};
-
-function getInfo(ID){
-    d3.json("samples.json").then((data) => {
-        var demoInfo = d3.select("#sample-metadata");
-        var metadata = data.metadata;
-        var results = metadata.filter(d => parseInt(d.id) === parseInt(ID))[0];
-        console.log(results);
-        demoInfo.html("");
-        Object.entries(results).forEach(([key, value]) => {
-            demoInfo.append("h6").text(`${key.toUpperCase()}: ${value}`);
-        });
-
-        var dataGauge = [
-            {
-                domain: {x:[0,1], y: [0,1]},
-                value: results.wfreq,
-                title: {text: `Weekly washing frequency`},
-                type: "indicator", 
-                mode: "gauge+number",
-                gauge: {axis: {range: [null, 10] },
-                            steps: [
-                                {range: [0,2], color: "FloralWhite" },
-                                {range: [2,4], color: "cornsilk"},
-                                {range: [4,6], color: "LightPink"},
-                                {range: [6,8], color: "PaleVioletRed"},
-                                {range: [8,10], color: "MediumVioletRed"}
-                            ]}
-            }
-        ];
-
-        var layoutGauge = {
-            width: 700,
-            height: 700
-        };
-        
-        Plotly.newPlot('gauge', dataGauge, layoutGauge);
-    });
-};
-
-function init() {
-    d3.json("samples.json").then((data) => {
-        var names = data.names; 
+        globalJson = data; 
+        var names = globalJson.names; 
         console.log(names);
         names.forEach((sample) => {
             dropDown
@@ -113,20 +16,29 @@ function init() {
           });
     });
     
-    top10bar(940);
+    // top10bar(940);
     getInfo(940);
 
 };
 
+function getInfo(ID) {  
+    var metadata = globalJson.metadata;
+    var results = metadata.filter(d => parseInt(d.id) === parseInt(ID))[0];
+    var demoInfo = d3.select("#sample-metadata");
+    console.log(results);
+    demoInfo.html("");
+    Object.entries(results).forEach(([key, value]) => {
+        demoInfo.append("h6").text(`${key.toUpperCase()}: ${value}`);
+    });
+};
+
 function optionChanged(change){
-    top10bar(change);
-    getInfo(change);
-    
+    // top10bar(change);
+    getInfo(change);   
 }; 
 
-
-
 init()
+
 
 
 
